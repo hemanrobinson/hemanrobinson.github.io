6c3fb66c1342bbca98efdb5c0f1b35eec5b0ac13
Heman Robinson <heman.robinson@gmail.com>
2022-12-16 14:07:49 -0500

    Add files via upload

---
+++ src/App.test.js
@@ -0,0 +1,25 @@
+import { act, render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+let container;
+
+beforeEach(() => {
+    container = document.createElement( "div" );
+    document.body.appendChild( container );
+});
+
+afterEach(() => {
+    document.body.removeChild( container );
+    container = null;
+});
+
+test( "renders App with childnodes", () => {
+    // Test first render and componentDidMount
+    act(() => {
+        ReactDOM.render(<App />, container );
+    });
+    const div = container.querySelector( "div" );
+    expect( div.className ).toBe( "" );
+    expect( div.childNodes.length ).toBe( 4 );
+});
---
+++ src/ReportCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card, CardHeader, CardMedia, CardContent, CardActionArea } from '@material-ui/core';
+
+// Report Card:  A Card with title, link, thumbnail, and text.
+function ReportCard({ title, href, img, text }) {
+    return (
+        <Card style={{maxWidth: 300}}>
+            <CardActionArea component="a" href={href}>
+                <CardHeader title={title} />
+                <CardMedia style={{height: 0, paddingTop: '66.67%'}} image={img} title={title} />
+                <CardContent>{text}</CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
+
+export default ReportCard;
