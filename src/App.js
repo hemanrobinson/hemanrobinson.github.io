f66aaee13fec9b4c077dd2d7c6c98f7656e8db04
Heman Robinson <heman.robinson@gmail.com>
2024-01-07 21:19:42 -0500

    consistent text

--- src/App.js
+++ src/App.js
@@ -33,16 +33,16 @@ function App() {
             <div className="Description">
                 <h2>Inspiration</h2>
                 <p>
-                On visualization, my main influences are <a href="https://www.stat.purdue.edu/~wsc/">Cleveland</a> and <a href="https://www.edwardtufte.com/tufte/">Tufte</a>, who emphasize the data and de-emphasize everything else.  <a href="https://www.perceptualedge.com">Few</a> is an expert on usability of visualizations, particularly of dashboards.  I believe in making visualizations as interactive as possible, to enable users to work efficiently, at their own speed.
-                </p>
-                <p className="center">
-                    <img title="Inspiration: Visualization" alt="Inspiration: Visualization" src="inspiration0.jpg"/>
-                </p>
-                <p>
                 On user interface design, my main influences are <a href="http://www.cs.umd.edu/users/ben/">Shneiderman</a> and <a href="https://www.nngroup.com/people/jakob-nielsen/">Nielsen</a>.  I believe in following standard design patterns, like <a href="https://material.io">Material Design</a>, to minimize the user's learning curve.
                 </p>
                 <p className="center">
                     <img title="Inspiration: Usability" alt="Inspiration: Usability" src="inspiration1.jpg"/>
+                </p>
+                <p>
+                On visualization, my main influences are <a href="https://www.stat.purdue.edu/~wsc/">Cleveland</a> and <a href="https://www.edwardtufte.com/tufte/">Tufte</a>, who emphasize the data and de-emphasize everything else.  <a href="https://www.perceptualedge.com">Few</a> is an expert on usability of visualizations, particularly of dashboards.  I believe in making visualizations as interactive as possible, to enable users to work efficiently, at their own speed.
+                </p>
+                <p className="center">
+                    <img title="Inspiration: Visualization" alt="Inspiration: Visualization" src="inspiration0.jpg"/>
                 </p>
                 <p>
                     Thanks for viewing!
