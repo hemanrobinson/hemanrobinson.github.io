f30640305e4913b9b352e55ba3462fa42affe365
Heman Robinson <heman.robinson@gmail.com>
2025-07-20 08:33:28 -0400

    Improve report card

--- src/ReportCard.js
+++ src/ReportCard.js
@@ -4,7 +4,7 @@ import { Card, CardHeader, CardMedia, CardContent, CardActionArea } from '@mui/m
 // Report Card:  A Card with title, link, thumbnail, and text.
 function ReportCard({ title, href, img, text }) {
     return (
-        <Card style={{maxWidth: 310}}>
+        <Card style={{maxWidth: 310, boxShadow: '1px 1px 5px 0px grey'}}>
             <CardActionArea component="a" href={href}>
                 <CardHeader title={title}/>
                 <CardMedia style={{height: 0, paddingTop: '66.67%'}} image={img} title={title} />
